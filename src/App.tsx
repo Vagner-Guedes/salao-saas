9351d15cbc4573562cf61546488176a8b69c3aef
Vagner Guedes <vagner.ads.guedes@gmail.com>
2026-02-26 16:22:59 -0300

    Configure React Router and application routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,7 @@
+import AppRoutes from "./routes"
+
 function App() {
-  return (
-    <div className="min-h-screen bg-gray-100 flex items-center justify-center">
-      <h1 className="text-3xl font-bold text-purple-600">
-        Tailwind funcionando 🎉
-      </h1>
-    </div>
-  )
+  return <AppRoutes />
 }
 
 export default App
--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,5 +1,25 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom"
+
+import Login from "../pages/Login"
+import Dashboard from "../pages/Dashboard"
+import Agenda from "../pages/Agenda"
+import Clients from "../pages/Clients"
+import Professionals from "../pages/Professionals"
+import Services from "../pages/Services"
+
 function AppRoutes() {
-    return null
-  }
-  
-  export default AppRoutes
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Login />} />
+        <Route path="/dashboard" element={<Dashboard />} />
+        <Route path="/agenda" element={<Agenda />} />
+        <Route path="/clients" element={<Clients />} />
+        <Route path="/professionals" element={<Professionals />} />
+        <Route path="/services" element={<Services />} />
+      </Routes>
+    </BrowserRouter>
+  )
+}
+
+export default AppRoutes
